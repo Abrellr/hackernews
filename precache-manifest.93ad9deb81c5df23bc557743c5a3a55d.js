cc857a3601f77a8ebbd82cbcf09ab015a2568b2b
Abrellr <69308389+Abrellr@users.noreply.github.com>
2020-12-23 14:50:18 +0100

    Updates

---
+++ precache-manifest.93ad9deb81c5df23bc557743c5a3a55d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d198a0d0d5dedcf8afd607b9e060e94",
+    "url": "/Abrellr/hackernews/index.html"
+  },
+  {
+    "revision": "28e3be958f9412b30529",
+    "url": "/Abrellr/hackernews/static/css/main.ad783688.chunk.css"
+  },
+  {
+    "revision": "0aba123750adaccef560",
+    "url": "/Abrellr/hackernews/static/js/2.258482c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Abrellr/hackernews/static/js/2.258482c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28e3be958f9412b30529",
+    "url": "/Abrellr/hackernews/static/js/main.58fba4a0.chunk.js"
+  },
+  {
+    "revision": "ce0a95aa791115c447fa",
+    "url": "/Abrellr/hackernews/static/js/runtime-main.66339ec9.js"
+  },
+  {
+    "revision": "fe2fde914118a7ea844599c31e96a36f",
+    "url": "/Abrellr/hackernews/static/media/background.fe2fde91.png"
+  }
+]);
